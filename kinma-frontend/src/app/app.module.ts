6c5e04adb73d0783f861fa2fc2938fa5f2d81cb0
Harryu <s0930322580@gmail.com>
2021-12-11 13:11:26 +0800

    footer added

--- kinma-frontend/src/app/app.module.ts
+++ kinma-frontend/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ProjectCardComponent } from './project-card/project-card.component';
 import { CategoryComponent } from './category/category.component';
 import { ClassicProductCartComponent } from './classic-product-cart/classic-product-cart.component';
 import { BlockHeaderComponent } from './block-header/block-header.component';
+import { FooterComponent } from './footer/footer.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -23,7 +24,8 @@ import { BlockHeaderComponent } from './block-header/block-header.component';
     ProjectCardComponent,
     CategoryComponent,
     ClassicProductCartComponent,
-    BlockHeaderComponent
+    BlockHeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
