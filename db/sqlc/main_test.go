46fb5f42d82a4a8c0334ea588ca57f9785e9bdd2
Harryu <s0930322580@gmail.com>
2022-02-18 14:47:32 +0800

    Load config from env

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -6,20 +6,20 @@ import (
 	"os"
 	"testing"
 
+	"github.com/kinmaBackend/util"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:password@localhost:5432/kinma_db?sslmode=disable"
-)
 var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	var err error
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load the env file:", err)
+	}
 	
-	testDB, err = sql.Open(dbDriver, dbSource)
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to the db:",err)
 	}
