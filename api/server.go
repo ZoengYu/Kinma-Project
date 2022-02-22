d9dc45fb3bca2b0ce35b44e9dbf96e12fdf7a72b
Harryu Wang <harryuwang@gmail.com>
2022-02-22 14:22:17 +0800

    login user return PASETO/JWT token

--- api/server.go
+++ api/server.go
@@ -1,46 +1,67 @@
 package api
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
 	db "github.com/kinmaBackend/db/sqlc"
+	"github.com/kinmaBackend/token"
+	"github.com/kinmaBackend/util"
 )
 
 type Server struct {
-	store 		*db.Store
-	router 		*gin.Engine
+	store 			*db.Store
+	tokenMaker 	token.Maker
+	router 			*gin.Engine
+	config      util.Config
 }
 
-func NewServer(store *db.Store) *Server{
-	server := &Server{store : store}
-	router := gin.Default()
+func NewServer(config util.Config, store *db.Store) (*Server, error){
+	//here we use AsymmetricPaseto token
+	tokenMaker, err := token.NewAsymmetricPasetoMaker()
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token maker: %w", err)
+	}
+
+	server := &Server{
+		store 		: store,
+		config		: config,
+		tokenMaker: tokenMaker,
+	}
 
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok{
 		v.RegisterValidation("currency",validCurrency)
 	}
-	//Restful API generated here
-	router.POST("/users", server.createUser)
+
+	server.setupRouter()
 	
-	router.POST("/accounts", server.createAccount)
-	router.GET("/accounts/:id", server.getAccount)
-	router.GET("/accounts", server.listAccount)
-
-	router.POST("/products", server.createProduct)
-	router.GET("/products/:id", server.getProduct)
-	router.GET("/products", server.listProduct)
-	router.PUT("/updateproduct/:id", server.updateProduct)
-
-	router.POST("/fundraise", server.createFundraise)
-	router.GET("/fundraise/:id", server.getFundraise)
-	router.PUT("/exitfundraise", server.exitFundraise)
-
-	router.POST("/transfer", server.createTransfer)
-	
-	server.router = router
-	return server
+	return server, nil
 }
 
+func (server *Server) setupRouter() {
+		router := gin.Default()
+		//Restful API generated here
+		router.POST("/users", server.createUser)
+		router.POST("/users/login", server.loginUser)
+		
+		router.POST("/accounts", server.createAccount)
+		router.GET("/accounts/:id", server.getAccount)
+		router.GET("/accounts", server.listAccount)
+	
+		router.POST("/products", server.createProduct)
+		router.GET("/products/:id", server.getProduct)
+		router.GET("/products", server.listProduct)
+		router.PUT("/updateproduct/:id", server.updateProduct)
+	
+		router.POST("/fundraise", server.createFundraise)
+		router.GET("/fundraise/:id", server.getFundraise)
+		router.PUT("/exitfundraise", server.exitFundraise)
+	
+		router.POST("/transfer", server.createTransfer)
+		server.router = router
+}
 // Start runs the HTTP server on a specific address.
 func (server *Server) Start(address string) error {
 	return server.router.Run(address)
