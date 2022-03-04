3a447c853cfdbcb7f0d2c9dfc9a226cd56314a39
Harryu Wang <harryuwang@gmail.com>
2022-03-04 18:06:01 +0800

    load env before migrate

--- start.sh
+++ start.sh
@@ -3,6 +3,8 @@
 set -e
 
 echo "run db migration"
+#migrate require the $DB_SOURCE variable to connect to db
+source /app/app.env
 /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
 
 echo "start the app"
