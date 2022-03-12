93e2594fa7c1545f96f6e8159733c5626a384402
Harryu Wang <harryuwang@gmail.com>
2022-03-12 13:01:08 +0800

    delete django redundancy file

--- website/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class WebsiteConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'website'
-    verbose_name = 'website-ui'
